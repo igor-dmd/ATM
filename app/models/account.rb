f9e325f3ba48daf38caec8107458a6cc455c9ea5
Igor de Medeiros <igordmed@MacBook-Pro.local>
2017-04-27 22:23:46 -0300

    create auto generated fields upon account creation

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,33 @@
 class Account < ApplicationRecord
   belongs_to :user
 
+  before_save :generate_fields
 
+  def generate_fields
+    set_limit
+    set_account_number
+    set_branch
+    set_token
+  end
+
+  def set_limit
+    self.limit = rand(100000..180000)
+  end
+
+  def set_token
+    self.token = Digest::SHA256.hexdigest self.account_number
+  end
+
+  def set_account_number
+    self.account_number = generate_sequence_number(5).insert(-2, '-')
+  end
+
+  def set_branch
+    self.branch = generate_sequence_number(4)
+  end
+
+  private
+  def generate_sequence_number(qty)
+    qty.times.map{rand(10)}.join
+  end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#save' do
+    context 'successfully' do
+      let (:account) {Account.create(user: FactoryGirl.create(:user))}
+
+      it 'creates a new account with auto generated data for limit field' do
+        expect(account.limit).not_to be_nil
+      end
+
+      it 'creates a new account with auto generated data for account_number field' do
+        expect(account.account_number).not_to be_nil
+      end
+
+      it 'creates a new account with auto generated data for branch field' do
+        expect(account.branch).not_to be_nil
+      end
+
+      it 'creates a new account with auto generated data for token field' do
+        expect(account.token).not_to be_nil
+      end
+    end
+  end
 end
