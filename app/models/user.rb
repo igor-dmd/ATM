206758a1e32a5faaf9edc7d4aa86154231a7fa4a
Igor de Medeiros <igordmed@MacBook-Pro.local>
2017-04-27 21:21:12 -0300

    create account when creating a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_one :address
   has_one :account
+
+  before_save :create_account
+
+  def create_account
+    self.account = Account.new
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#save' do
+    context 'successfully' do
+      it 'creates an account for the user' do
+        user =  FactoryGirl.build(:user)
+        user.save
+
+        expect(user.account).not_to be_nil
+      end
+    end
+  end
 end
