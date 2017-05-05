0076e3a74f30a9950dec416ee946cf601fe4c7d9
Igor de Medeiros <igordmed@MacBook-Pro.local>
2017-05-05 14:56:08 -0300

    change routes names

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   put 'users/:id', to: 'users#update'
   delete 'users/:id', to: 'users#destroy'
 
-  put 'limit/:user_id/update/limit', to: 'limits#update'
-  post 'deposit/:user_id', to: 'deposits#create'
-  post 'transfer/:user_id', to: 'transfers#create'
-  get 'account/:user_id/statement', to: 'accounts#index'
-  get 'account/:user_id/balance', to: 'accounts#show'
-  post 'withdraw/:user_id/request', to: 'withdraws#create'
-  post 'withdraw/:user_id/confirm', to: 'withdraws#update'
+  put 'limits/:user_id', to: 'limits#update'
+  post 'deposits/:user_id', to: 'deposits#create'
+  post 'transfers/:user_id', to: 'transfers#create'
+  get 'accounts/:user_id/statement', to: 'accounts#index'
+  get 'accounts/:user_id/balance', to: 'accounts#show'
+  post 'withdraws/:user_id/request', to: 'withdraws#create'
+  post 'withdraws/:user_id/confirm', to: 'withdraws#update'
 end
