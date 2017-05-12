7d8ed0edd1e1f50a4f6a08cbe5efaa7b87dd3012
Igor de Medeiros <igormedeiros0@gmail.com>
2017-05-12 09:22:33 -0300

    create user db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'ffaker'
+
+10.times do
+  user = User.create!( full_name: FFaker::NameBR.name,
+                       cpf: 11.times.map{rand( 10 )}.join,
+                      birthday_date: FFaker::Time.between( 50.years.ago, 20.years.ago ),
+                      gender: rand( 2 ),
+                      password: Digest::SHA256.hexdigest( FFaker::Internet.password ) )
+
+  address = Address.create!( street_name: FFaker::AddressBR.street + FFaker::AddressBR.building_number,
+                       city: FFaker::AddressBR.city,
+                       state: FFaker::AddressBR.state,
+                       country: 'Brazil',
+                             user_id: user.id )
+
+  user.address = address
+  user.save!
+end
+
